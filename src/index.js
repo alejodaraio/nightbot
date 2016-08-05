9c01c1171ede43a6c7fdf857e1e2b795978065b2
adaraio <alejo_daraio@artear.com>
2016-08-05 12:54:54 -0300

    added index root

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var http_app = express();
-var port = process.env.PORT || 8080;
+
+http_app.set('port', (process.env.PORT || 5000));
 
 http_app.get('/', function(req, res){
    res.send('nightbot!');
 });
 
-http_app.listen(port, function () {
+http_app.listen(http_app.get('port'), function () {
     console.log('http is UP!');
 });
 
