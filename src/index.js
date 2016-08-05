dbe0a69726c651a1f6cf03f6ff63d9ccc6d1f8b8
adaraio <alejo_daraio@artear.com>
2016-08-05 11:03:12 -0300

    added: http with express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var http_app = express();
+var port = process.env.PORT || 8080;
 
 http_app.get('/', function(req, res){
    res.send('nightbot!');
 });
 
-http_app.listen(80, function () {
+http_app.listen(port, function () {
     console.log('http is UP!');
 });
