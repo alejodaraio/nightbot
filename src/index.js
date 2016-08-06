73207090cb83b9114c72ac99000b065fda9fd29c
alejodaraio <alejodaraio@gmail.com>
2016-08-06 20:57:37 -0300

    added request module

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var http_app = express();
+var request = require('request');
 
 const NIGHTBOT_API = 'https://beta.nightbot.tv';
+const NIGHTBOT_CLIENT_ID = '15ba6d3a60e6e5cd684311ea1d86904d';
+const NIGHTBOT_CLIENT_SECRET = '3830fbadc4bf402a20ed5e1442a65f74';
 const NIGHTBOT_REDIRECT = 'https://nightbot.herokuapp.com/currentsong';
+const NIGHTBOT_OAUTH2_TOKEN = 'https://api.nightbot.tv/oauth2/token';
 const NIGHTBOT_REQUEST_QUEUE = 'https://api.nightbot.tv/1/song_requests/queue';
 
 //init settings
@@ -14,12 +18,16 @@ http_app.set('view engine', 'ejs');
 //configure the routes
 http_app.get('/', function (req, res) {
     res.render('pages/index', {
-        nightbot_oauth: nightBot_autorize_url('15ba6d3a60e6e5cd684311ea1d86904d')
+        nightbot_oauth: nightBot_autorize_url(NIGHTBOT_CLIENT_ID)
     });
 });
 
-http_app.get('/currentsong', function(req, res){
-    res.send('Lalalal Song' + req.query.code);
+http_app.get('/currentsong', function (req, res) {
+    res.send('Current Song');
+});
+
+http_app.get('/oauth2token', function (req, res) {
+    nightBot_oauth2token(req.query.code);
 });
 
 http_app.listen(http_app.get('port'), function () {
@@ -31,6 +39,24 @@ function nightBot_autorize_url(client_id) {
     return NIGHTBOT_API + '/oauth2/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + NIGHTBOT_REDIRECT;
 }
 
-function nightBot_requestSong(code) {
+function nightBot_oauth2token(code) {
+
+    request.post(NIGHTBOT_OAUTH2_TOKEN,
+        {
+            form: {
+                client_id: NIGHTBOT_CLIENT_ID,
+                client_secret: NIGHTBOT_CLIENT_SECRET,
+                grant_type: 'authorization_code',
+                redirect_uri: 'https://nightbot.herokuapp.com/currentsong',
+                code: code
+            }
+        },
+        function (err, httpResponse, body) {
+            console.log(body);
+        }
+    );
+}
+
+function nightBot_requestSong(token) {
     var url = '';
 }
